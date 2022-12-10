cda5b03da8afd506daca3191f80ca4ee621c8b49
srini-equitasit <srini@equitasitinc.com>
2022-12-11 04:36:27 +0530

    Update vpc.tf

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -1,7 +1,31 @@
 resource "aws_vpc" "main" {
+
   cidr_block = "10.0.0.0/16"
+
+# make your instance shared on host
+  instance_tenancy = "default"
+
+# required for EKS. Enable / disable DNS support in the VPC 
+  enable_dns_support = true
+
+# required for EKS. Enable / disable DNS hostnames in the VPC 
+  enable_dns_hostnames = true
+
+#  Enable/Disable classic link
+  enable_classiclink = false
+
+#  Enable/Disable classic link dns support
+  enable_classiclink_dns_support = false
+
+  assign_generated_ipv6_cidr_block = false
 
   tags = {
     Name = "main"
   }
+}
+
+output "vpc_id" {
+  value=aws_vpc.main.id
+  description = "vpc id"
+  sensitive = false
 }
