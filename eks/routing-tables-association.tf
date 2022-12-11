b5f39198ae1a287eadbad6854b2a78eeabda220c
srini-equitasit <srini@equitasitinc.com>
2022-12-11 12:46:17 +0530

    Update routing-tables-association.tf

--- eks/routing-tables-association.tf
+++ eks/routing-tables-association.tf
@@ -21,7 +21,7 @@ resource "aws_route_table_association" "public_2_rta" {
 resource "aws_route_table_association" "private_1_rta" {
   
 #  The ID of subnet, which will apply the route
-  subnet_id = aws_subnet.private_1-us-east-2a
+  subnet_id = aws_subnet.private_1-us-east-2a.id
 
 # public route table id
   route_table_id = aws_route_table.private_1_rt.id
@@ -30,7 +30,7 @@ resource "aws_route_table_association" "private_1_rta" {
 resource "aws_route_table_association" "private_2_rta" {
   
 #  The ID of subnet, which will apply the route
-  subnet_id = aws_subnet.private_2-us-east-2b
+  subnet_id = aws_subnet.private_2-us-east-2b.id
 # public route table id
   route_table_id = aws_route_table.private_2_rt.id
 }
