9a8332e604d5c44108d2d6545e1c265380320c48
srini-equitasit <srini@equitasitinc.com>
2022-12-12 03:10:35 +0530

    Update alb-deployment.tf

--- eks/alb-deployment.tf
+++ eks/alb-deployment.tf
@@ -20,7 +20,7 @@ provider "kubernetes" {
   host = aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
-  load_config_file       = false
+#   load_config_file       = false
 
 }
 
