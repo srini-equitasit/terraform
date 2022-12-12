0311073b78b1b862f8f3329d882e7fc70ff72981
srini-equitasit <srini@equitasitinc.com>
2022-12-12 15:40:34 +0530

    alb config

--- eks/alb/2-eks.tf
+++ eks/alb/2-eks.tf
@@ -69,3 +69,14 @@ module "eks" {
 
   
 }
+# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
+
+
+}
